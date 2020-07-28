56741e8dbcca49289e48eea0dcf7c31464b3873e
RaphaelWirawan <alessandrowirawan@gmail.com>
2020-07-28 22:44:30 +0700

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.teal[400]),
         home: Scaffold(
-            appBar: AppBar(title: Text('WordPair Generator')), body: Center(child: Text('Hello World'))));
+            appBar: AppBar(title: Text('WordPair Generator')),
+            body: Center(child: Text(wordPair.asPascalCase))));
   }
 }
